7584fa370ef7f6cf64dc084fca7817789cc443b4
discover-pangea <46792513+discover-pangea@users.noreply.github.com>
2019-01-26 17:33:26 +0300

    test_send_new

---
+++ test_send/js/formspree.js
@@ -0,0 +1,30 @@
+$(document).on('submit', '#contact-form1', function(e) {
+    e.preventDefault();
+    var replyto = $('input[name=_replyto]'),
+        replyto_text = replyto.val(),
+        subject = $('input[name=_subject]'),
+        subject_text = subject.val(),
+        body = $('textarea[name=body]'),
+        body_text = body.val();
+
+    $.ajax({
+        url: "https://formspree.io/txcdxevubase@mail.ru",
+        method: "POST",
+        data: {
+            _replyto: replyto_text,
+            _subject: subject_text,
+            message: body_text
+        },
+        dataType: "json",
+        beforeSend: function( xhr ) {
+            spinner.show();
+      }
+    }).done(function(){
+        $('#contacts-success-wrapper').show();
+    }).fail(function() {
+        $('#contacts-error-wrapper').show();
+    }).always(function() {
+        spinner.hide();
+        $('#contacts-wrapper').remove();
+    });
+});
